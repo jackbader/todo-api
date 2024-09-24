962069c98a1be0cbfa21fa566c5e72ae9e30d49f
Jack Bader <jack@culpritunderwear.com>
2024-09-23 22:02:19 -0400

    adding port from process.env for heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Heroku's PORT or default to 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`); // Log the correct port
 });
